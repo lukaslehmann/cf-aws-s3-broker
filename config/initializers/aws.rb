fd542b7c5c426c2010b1a2c12b926e494aa28c55
lukaslehmann <lukas92lehmann@gmail.com>
2013-11-26 11:24:28 -0800

    demo aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,12 +1,5 @@
-# de mit wenig berechtigugngen
   # AWS.config({
-  #   :access_key_id => 'AKIAI7DW3HAJ3N57MWNA',
-  #   :secret_access_key => 't0R1bpwWTAvj91VbonrNIRsd4eN7axe/1Bd64O98',
+  #   :access_key_id => 'your-ID',
+  #   :secret_access_key => 'your-access-key',
   # })
-
-
-  AWS.config({
-    :access_key_id => 'AKIAJ6SJDEP2MJDSAZKA',
-    :secret_access_key => 'y0nBWc3wVasc3MYlV1o7r8mbOpAEthIXQ9I5O4Mf',
-  })
 
